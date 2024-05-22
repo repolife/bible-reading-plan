3ab325251778eecfcd3024ab67f2d8f3d57ca49e
david[user] <speedy@Studio>
2024-05-22 10:38:12 -0500

    set alpabet

--- src/FilteredReadingPlan.jsx
+++ src/FilteredReadingPlan.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import readingPlan from "../bible_plan.json";
 import axios from "axios";
 import ReadingItem from "./ReadingItem";
 import Navbar from "./NavBar";
 
 const FilteredReadingPlan = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split("T")[0]); // Default to today's date
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split("T")[0]);
 
   const handleDateChange = (event) => {
     setSelectedDate(event.target.value);
@@ -26,25 +26,37 @@ const FilteredReadingPlan = () => {
   const startOfWeek = getStartOfWeek(selectedDate);
   const endOfWeek = getEndOfWeek(selectedDate);
 
-  const weeklyReadings = readingPlan.filter((entry) => {
-    const entryDate = new Date(entry.date);
-    return entryDate >= startOfWeek && entryDate <= endOfWeek;
-  });
+  const weeklyReadings = useMemo(() => {
+    if (readingPlan.lenght <= 0) return [];
+
+    let filteredPlan = [];
+    readingPlan.filter((entry) => {
+      const entryDate = new Date(entry.date);
+
+      if (entryDate === undefined) {
+        console.log("empty", entry);
+      }
+      if (entryDate >= startOfWeek && entryDate <= endOfWeek) {
+        filteredPlan = [...filteredPlan, entry];
+      }
+    });
+    return filteredPlan;
+  }, [selectedDate, readingPlan]);
 
   return (
     <div>
       <Navbar />
-      <h2>{new Date(selectedDate).toLocaleDateString()}</h2>
+      <h2>{selectedDate}</h2>
       <h1>Ozark Reading Plan</h1>
       <label htmlFor="date-picker">Select a date: </label>
       <input type="date" id="date-picker" value={selectedDate} onChange={handleDateChange} />
-      {weeklyReadings.length > 0 ? (
+      {weeklyReadings && weeklyReadings.length > 0 ? (
         <div>
           {weeklyReadings.map((reading, index) => {
             return (
               reading.passage && (
-                <div key={index}>
-                  <h3>{new Date(reading.date).toLocaleDateString()}</h3>
+                <div key={index} style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                  {/* <h3>{new Date(reading.date).toLocaleDateString()}</h3> */}
                   {reading.passage.split("; ").map((passage, i) => (
                     <ReadingItem key={i} passage={passage} />
                   ))}

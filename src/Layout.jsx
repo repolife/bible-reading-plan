512a2d445b82503f468fa0fecbef6fe0985a3ddc
david[user] <speedy@Studio>
2024-05-22 13:29:54 -0500

    Remove width

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Layout({ children }) {
-  return <div style={{ height: "100vh", width: "100vw" }}>{children}</div>;
+  return <div style={{ height: "100vh" }}>{children}</div>;
 }
